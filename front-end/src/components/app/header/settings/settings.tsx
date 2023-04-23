a1183d6c0d39a47ad4439635158be2cf27f9864a
pboidin <pierrelouisboidin@gmail.com>
2023-04-23 11:51:50 +0200

    add css header

--- front-end/src/components/app/header/settings/settings.tsx
+++ front-end/src/components/app/header/settings/settings.tsx
@@ -9,7 +9,7 @@ function Settings() {
       <Link href={`/`}>
         <div className={settingsStyles.ctn__set}>
           <p className={settingsStyles.ctn__set__p}>
-            <FiSettings size={25} />
+            <FiSettings className={settingsStyles.fi__settings}/>
           </p>
         </div>
       </Link>
