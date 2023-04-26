38d7d5bbdfb7ea9b2c3cab2cc319446da3a3f0e1
mlarboul <mlarboul@student.42.fr>
2023-04-26 04:18:36 +0200

    useGuards working with jwToken

--- back-end/src/profile/profile.controller.ts
+++ back-end/src/profile/profile.controller.ts
@@ -1,3 +1,4 @@
+import { AuthService } from '@/auth/auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Controller } from '@nestjs/common';
 import { Get, Req, Res, UseGuards } from '@nestjs/common';
@@ -5,15 +6,18 @@ import { CookieOptions, Request, Response } from 'express';
 
 @Controller('profile')
 export class ProfileController {
-
-  // @UseGuards(AuthGuard('42'))
   @Get()
-    sendInfo(@Req() req: Request, @Res() res: Response): any {
-      const profile = {
-        username: "Matt",
-      }
-      console.log(req.headers);
+  @UseGuards(AuthGuard('jwt'))
+  sendInfo(@Req() req: Request, @Res() res: Response): any {
+    // Access the user object from the request
+    const user = req.user;
+    console.log(user);
+    // You can now use the user information in your response
+    const profile = {
+      username: 'test', // Assuming the user object has a 'username' property
+      // Add any other user properties you'd like to include in the response
+    };
 
-      return res.status(200).json(profile);
-    }
+    return res.status(200).json(profile);
+  }
 }
