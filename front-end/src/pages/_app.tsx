b4952d575acc88f68ae84d09f63cce3271ff2a17
pboidin <pierrelouisboidin@gmail.com>
2023-04-26 21:07:35 +0200

    add the dashboard page with a css but non responsive

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -2,6 +2,11 @@ import AuthProvider from "@/providers/auth/auth.provider";
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+	weight: '300',
+	subsets: ['latin'] })
 
 function ft_transcendence({ Component, pageProps }: AppProps) {
   return (
