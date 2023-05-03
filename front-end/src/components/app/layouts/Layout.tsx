1ee2e644a0cccf343d3798d01f694e792b460730
Karl MAZIER <mazier.karl@gmail.com>
2023-05-03 21:09:25 +0200

    Refactor Layout component

--- front-end/src/components/app/layouts/Layout.tsx
+++ front-end/src/components/app/layouts/Layout.tsx
@@ -1,52 +1,48 @@
+import React from 'react';
 import Head from 'next/head';
-import React, { ReactElement } from 'react';
-import Header from '../header/header';
-import Navbar from '../navbar/navbar';
+import Navbar from '@/components/app/navbar/navbar';
+import Header from '@/components/app/header/header';
+
+export enum Mode {
+  WITH_LAYOUT = 'withLayout',
+  WITHOUT_LAYOUT = 'withoutLayout',
+}
 
 interface LayoutProps {
   children: React.ReactNode;
   className?: string;
   title?: string;
+  loading?: boolean;
+  mode?: Mode;
 }
 
-function Layout({ title, children, className }: LayoutProps): ReactElement {
-  if (title === 'Connexion') {
-    return (
-      <>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>
-            {title ? `${title} - ft_transcendence` : 'ft_transcendence'}
-          </title>
-        </Head>
-
-        <main className={className}>{children}</main>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>
-            {title ? `${title} - ft_transcendence` : 'ft_transcendence'}
-          </title>
-        </Head>
-
-        <Navbar />
-        <Header {...{ title }} />
-        <main className={className}>{children}</main>
-      </>
-    );
-  }
+function Layout({
+  title,
+  children,
+  className,
+  mode = Mode.WITH_LAYOUT,
+}: LayoutProps): React.ReactElement {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>
+          {title ? `${title} - ft_transcendence` : 'ft_transcendence'}
+        </title>
+      </Head>
+      {mode === Mode.WITH_LAYOUT ? (
+        <>
+          <Navbar />
+          <Header {...{ title }} />
+        </>
+      ) : null}
+      <main className={className}>{children}</main>
+    </>
+  );
 }
 
 export default Layout;
--- front-end/src/pages/login.tsx
+++ front-end/src/pages/login.tsx
@@ -1,4 +1,4 @@
-import Layout from '@/components/app/layouts/Layout';
+import Layout, { Mode } from '@/components/app/layouts/Layout';
 import LoginContent from '@/components/login/LoginContent';
 import { withPublic } from '@/providers/auth/auth.routes';
 import { NextPage } from 'next';
@@ -8,6 +8,7 @@ const Login: NextPage = () => {
     <Layout
       className="flex items-center justify-center h-screen bg-dark-purple"
       title="Connexion"
+      mode={Mode.WITHOUT_LAYOUT}
     >
       <LoginContent />
     </Layout>
