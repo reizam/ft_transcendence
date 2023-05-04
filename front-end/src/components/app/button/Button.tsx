edd80db40869953cd4e999452dbac17f1746e2d0
jmazoyer <jmazoyer@student.42.fr>
2023-05-04 21:49:01 +0200

    Update Button & ProfileCard

--- front-end/src/components/app/button/Button.tsx
+++ front-end/src/components/app/button/Button.tsx
@@ -1,24 +1,36 @@
 import React, { useState } from 'react';
 import styleButton from './Button.module.css';
 
-interface ButtonProps {
+// How to give ButtonProps a defined type in the Parent component
+// type MyFunctionType = (arg1: number, arg2: string) => void;
+
+// const myButtonProps: ButtonProps<MyFunctionType> = {
+//   initialName: 'My Button',
+//   setHasClicked: (arg1: number, arg2: string) => {
+//     // Your implementation here
+//   },
+// };
+
+interface ButtonProps<T = (...args: never[]) => void> {
   initialName: string;
   onClickName?: string;
+  onClick?: T;
 }
 
-function Button(props: ButtonProps) {
-  const { initialName, onClickName } = props;
+function Button({ initialName, onClickName, onClick }: ButtonProps) {
   const [name, setName] = useState(initialName);
 
+  console.log('name =', name);
   return (
     <>
       <button
         className={styleButton.style__button}
-        onClick={() =>
+        onClick={() => {
           setName(
             !!onClickName && name === initialName ? onClickName : initialName
-          )
-        }
+          );
+          !!onClick && onClick();
+        }}
       >
         {name}
       </button>
--- front-end/src/components/profile/cards/ProfileCard.tsx
+++ front-end/src/components/profile/cards/ProfileCard.tsx
@@ -1,27 +1,40 @@
+import React, { useState } from 'react';
 import Button from '@/components/app/button/Button';
 import ToggleSwitch from '@/components/app/toggle/ToggleSwitch';
 import ProfileAvatar from '@/components/profile/avatar/ProfileAvatar';
 import UserInfo from '@/components/profile/sections/UserInfoSection';
 import { ProfileData } from '@/components/profile/types/profile.type';
 import dashStyles from '@/styles/dash.module.css';
-import { ReactElement } from 'react';
 
 interface ProfileDataProps {
   profileData: ProfileData;
   canEdit: boolean;
 }
 
-function ProfileCard({ profileData, canEdit }: ProfileDataProps): ReactElement {
+function ProfileCard({
+  profileData,
+  canEdit,
+}: ProfileDataProps): React.ReactElement {
+  const [isEditing, setIsEditing] = useState(false);
+  const onClick = () => setIsEditing(!isEditing);
+
+  console.log('isEditing =', isEditing);
   return (
     <div className={dashStyles.dash__profile}>
-      <ProfileAvatar src={profileData.profilePicture} canEdit={canEdit} />
+      <ProfileAvatar
+        src={profileData.profilePicture}
+        canEdit={canEdit}
+        // isEditing={isEditing}
+      />
       <UserInfo
         firstName={profileData.firstName}
         lastName={profileData.lastName}
         username={profileData.username}
+        // isEditing={isEditing}
       />
-      {canEdit && <ToggleSwitch checked={profileData.has2FA} />}
-      <Button />
+      {canEdit} &&{' '}
+      <ToggleSwitch checked={profileData.has2FA} /* isEditing={isEditing}*/ />
+      <Button initialName="Edit" onClickName="Save" onClick={onClick} />
     </div>
   );
 }
