03215c2f4cd8cc441d2e15e68cc6278c1f4f4efc
jmazoyer <jmazoyer@student.42.fr>
2023-05-10 15:59:07 +0200

    Update the Prisma schema to match the Profile page

--- front-end/src/components/profile/avatar/ProfileAvatar.tsx
+++ front-end/src/components/profile/avatar/ProfileAvatar.tsx
@@ -18,16 +18,14 @@ function ProfileAvatar({
   const handleFileUpload = async (
     e: React.ChangeEvent<HTMLInputElement>
   ): Promise<void> => {
-    e.preventDefault();
-
     const MAX_FILE_SIZE = 1024;
     const ALLOWED_TYPE = ['image/jpg', 'image/jpeg'];
 
+    e.preventDefault();
     if (!e.target.files || e.target.files.length == 0) {
       toast.error('Please choose a file');
       return;
     }
-
     if (e.target.files.length > 1) {
       toast.error('Too much files selected');
       return;
@@ -53,11 +51,9 @@ function ProfileAvatar({
       const fileAsDataURL = fileReader.result as string;
       mutate({ profilePicture: fileAsDataURL });
     };
-
     fileReader.onerror = (): void => {
       toast.error('Error reading the file!');
     };
-
     fileReader.readAsDataURL(selectedFile);
   };
 
@@ -84,16 +80,6 @@ function ProfileAvatar({
           }}
         />
       </label>
-      {/* {canEdit && (
-        <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 hover:cursor-pointer rounded-full">
-          <BasicInput
-          type="file"
-          className="invisible"
-          accept="image/*"
-          onChange={handleFileUpload}
-          />
-          </div>
-        )} */}
     </div>
   );
 }
