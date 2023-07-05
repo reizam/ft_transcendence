41176fa6d9f484f18fb82a63c0b7ac4135c18e0b
PLB <pierrelouisboidin@gmail.com>
2023-07-05 16:55:23 +0200

    Update the size of the profile picture Update the result css after the game

--- front-end/src/components/game/GameResult.tsx
+++ front-end/src/components/game/GameResult.tsx
@@ -1,4 +1,5 @@
 import dashStyles from '@/styles/dash.module.css';
+import gameStyles from '@/styles/game.module.css';
 
 interface GameResultProps {
   winner: {
@@ -10,16 +11,21 @@ interface GameResultProps {
 
 export const GameResult = ({ winner }: GameResultProps): JSX.Element => {
   return (
-    <div className="flex flex-wrap">
-      <div className="flex justify-center">
+    <div className={gameStyles.ctn__result}>
+      <div className={gameStyles.ctn__result_img}>
         <img
           className={dashStyles.img__prof}
           src={winner.profilePicture}
           alt="Winner picture"
         />
       </div>
-      <div className="flex m-auto">
-        <h2>{winner.username} has won the game!</h2>
+      <div className={gameStyles.ctn__result_txt}>
+        <div className={gameStyles.ctn__result_txt_title}>
+          <h2 className={gameStyles.result_h2}>{winner.username}</h2>
+        </div>
+        <div className={gameStyles.ctn__result_txt_text}>
+          <p> has won the game!</p>
+        </div>
       </div>
     </div>
   );
