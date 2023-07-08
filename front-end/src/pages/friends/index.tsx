8d90b8cbb8009acaa60a0186708a5f46e26f1909
mlarboul <mlarboul@student.42.fr>
2023-07-08 17:49:34 +0200

    Fix(friends): Recharges only components that change
    The page friends was the only one that seems to reload all the
    components. Now, only friends component is load when you are going from
    a page to another page.

--- front-end/src/pages/friends/index.tsx
+++ front-end/src/pages/friends/index.tsx
@@ -1,35 +1,31 @@
 import { useGetFriends } from '@/api/friends/friends.get.api';
 import Layout from '@/components/app/layouts/Layout';
 import LoadingScreen from '@/components/app/screen/LoadingScreen';
+import UsersList from '@/components/friends/FriendsList';
 import { withProtected } from '@/providers/auth/auth.routes';
 import { ReactElement } from 'react';
 import FrStyles from '@/styles/friends.module.css';
-import FriendsList from '@/components/friends/FriendsList';
 
 function Friends(): ReactElement {
   const { data, isLoading, isError } = useGetFriends();
-
-  if (isLoading) return <LoadingScreen />;
-
-  if (isError || !data) return <p>No profile data</p>;
 
   return (
     <Layout title="Friends">
       {isLoading ? (
         <LoadingScreen />
-      ) : data ? (
+      ) : isError || !data ? (
+        <p>No profile data</p>
+      ) : (
         <>
           <div className={FrStyles.ctn__friends_section}>
             <div className={FrStyles.ctn__friends}>
-              <FriendsList users={data.friends} isFriend={true} />
+              <UsersList users={data.friends} isFriend={true} />
             </div>
             <div className={FrStyles.ctn__friends}>
-              <FriendsList users={data.nonFriends} isFriend={false} />
+              <UsersList users={data.nonFriends} isFriend={false} />
             </div>
           </div>
         </>
-      ) : (
-        <p>No data</p>
       )}
     </Layout>
   );
