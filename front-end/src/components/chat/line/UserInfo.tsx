a77e522fe6de36ac981149a42057c8ee7852aafe
jmazoyer <jmz.mazoyer@gmail.com>
2023-07-21 16:09:40 +0200

    Fix(Chat): Add a link to the profile on each username

--- front-end/src/components/chat/line/UserInfo.tsx
+++ front-end/src/components/chat/line/UserInfo.tsx
@@ -2,6 +2,7 @@ import chatStyles from '@/styles/chat.module.css';
 import { FaCrown } from 'react-icons/fa';
 import { ReactElement } from 'react';
 import { IChatUser } from '@/api/channel/channel.types';
+import Link from 'next/link';
 
 interface UserInfoProps {
   user: IChatUser;
@@ -11,7 +12,7 @@ interface UserInfoProps {
 function UserInfo({ user, isOwner }: UserInfoProps): ReactElement {
   return (
     <div className={chatStyles.ctn_user_name}>
-      {user?.username}
+      <Link href={`/profile/${user.id}`}>{user?.username}</Link>
       {isOwner ? (
         <>
           &nbsp;&nbsp;
