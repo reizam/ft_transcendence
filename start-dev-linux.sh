bb25a566aa22ec0f3b94762b632ab230d4316858
Matthieu <mlarboul@student.42.fr>
2023-07-24 18:02:24 +0200

    local

--- start-dev-linux.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f ./docker/dev/docker-compose.yml -p ft_transcendence_dev down && docker-compose -f ./docker/dev/docker-compose.yml -p ft_transcendence_dev up --build;
--- start-prod-linux.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f ./docker/prod/docker-compose.yml -p ft_transcendence_prod down && docker-compose -f ./docker/prod/docker-compose.yml -p ft_transcendence_prod up --build;
